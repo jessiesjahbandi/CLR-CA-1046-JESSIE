6fe25667f83c169cf3197b337a32f265f519c334
Jessie Sjahbandi <112393481+jessiesjahbandi@users.noreply.github.com>
2024-04-19 18:54:54 +0700

    update.php

--- update.php
+++ update.php
@@ -29,6 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </head>
 
 <body>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <div class="flex items-center justify-center min-h-screen bg-gray-100">
         <div class="px-8 py-6 mt-4 text-left bg-white shadow-lg">
             <h3 class="text-2xl font-bold text-center">Ubah Kontak</h3>
@@ -37,15 +38,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <input type="hidden" name="id" value="<?= $kontak['id'] ?>">
                     <label for="no_hp" class="block">Nomor HP:</label>
                     <input type="text" name="no_hp" id="no_hp" required value="<?= $kontak['no_hp'] ?>"
-                        class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+                        class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600">
                 </div>
                 <div class="mt-4">
                     <label for="owner" class="block">Owner:</label>
                     <input type="text" required value="<?= $kontak['owner'] ?>" name="owner" id="owner" required
-                        class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+                        class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600">
                 </div>
                 <div class="flex items-baseline justify-between">
-                    <input type="submit" value="Ubah" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
+                    <input type="submit" value="Ubah" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600">
                     <button
                     class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                         <a href="index.php">Kembali</a>
