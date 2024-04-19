64d31fbbaa12472be0047e94b89a82efc90896f5
Jessie Sjahbandi <112393481+jessiesjahbandi@users.noreply.github.com>
2024-04-19 21:03:38 +0700

    update.php

--- update.php
+++ update.php
@@ -24,6 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="output.css" />
     <title>Ubah Kontak</title>
 </head>
@@ -31,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <body>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <div class="flex items-center justify-center min-h-screen bg-gray-100">
-        <div class="px-8 py-6 mt-4 text-left bg-white shadow-lg">
+        <div class="px-8 py-6 mt-4 rounded-lg text-left bg-white shadow-lg">
             <h3 class="text-2xl font-bold text-center">Ubah Kontak</h3>
             <form action="update.php" method="post" class="mt-4">
                 <div>
@@ -45,18 +46,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <input type="text" required value="<?= $kontak['owner'] ?>" name="owner" id="owner" required
                         class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600">
                 </div>
-                <div class="flex items-baseline justify-between">
-                    <input type="submit" value="Ubah" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600">
-                    <button
-                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                        <a href="index.php">Kembali</a>
-                    </button>
+                <div class="flex items-center justify-between mt-4 gap-4">
+                    <input type="submit" value="Ubah" class="w-1/2 px-4 py-2 text-white bg-gradient-to-br from-[#e5a046] to-[#e5a046] hover:bg-blue-700 focus:outline-none focus:ring-1 focus:ring-gray-600 rounded-md">
+                    <a href="index.php" class="inline-block w-1/2 px-4 py-2 text-center text-white bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-1 focus:ring-gray-600 rounded-md">
+                        Kembali
+                    </a>
                 </div>
             </form>
         </div>
     </div>
-
-
 </body>
 
 </html>
