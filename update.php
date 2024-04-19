84b77e071c38109b8d627c5bdcce0f3fd5bd7a69
Jessie Sjahbandi <112393481+jessiesjahbandi@users.noreply.github.com>
2024-04-19 18:42:09 +0700

    update.php

--- update.php
+++ update.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <div class="flex items-center justify-center min-h-screen bg-gray-100">
         <div class="px-8 py-6 mt-4 text-left bg-white shadow-lg">
             <h3 class="text-2xl font-bold text-center">Ubah Kontak</h3>
-            <form action="ubah.php" method="post" class="mt-4">
+            <form action="update.php" method="post" class="mt-4">
                 <div>
                     <input type="hidden" name="id" value="<?= $kontak['id'] ?>">
                     <label for="no_hp" class="block">Nomor HP:</label>
