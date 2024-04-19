08f5038ad0be77e2f7f13c9c55bc0e5ee3a34fc4
Jessie Sjahbandi <112393481+jessiesjahbandi@users.noreply.github.com>
2024-04-19 21:04:26 +0700

    create.php

--- create.php
+++ create.php
@@ -18,6 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="output.css" />
     <title>Tambahkan Kontak</title>
 </head>
@@ -45,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     </button>
                     <button type="button" id="btnKembali"
                     class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                        Kembali
+                        <a href="index.php">Kembali</a>
                     </button>
                 </div>
             </form>
